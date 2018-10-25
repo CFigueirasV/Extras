3a168be04970613f0c4e37172a2b7d8ef31be00c
cfigueirasvalverde <cfigueirasvalverde@nautilus20.danielcastelao.org>
2018-10-25 10:10:52 +0200

    Extras

---
+++ Exemploide_1/src/exemploide/Exemploide.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemploide;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Exemploide {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {System.out.println("Un saúdo a DAM");//println salto de liña
+        // TODO code application logic here
+    }
+    
+}
