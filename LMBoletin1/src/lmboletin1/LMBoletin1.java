6b0b6511b37167317a6bd3ba23519eecb87f9e46
cfigueirasvalverde <cfigueirasvalverde@nautilus20.danielcastelao.org>
2018-10-25 10:14:33 +0200

    Extras

---
+++ LMBoletin1/src/lmboletin1/LMBoletin1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lmboletin1;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class LMBoletin1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
