2f45fbad440c01d03e4c6b17f53909cda72d1958
cfigueirasvalverde <cfigueirasvalverde@nautilus20.danielcastelao.org>
2018-10-25 10:10:17 +0200

    Extras

---
+++ Exemploide/src/exemploide/Exemploide.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemploide;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Exemploide {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        System.out.println("Un\nsaudo\na\tDAM");//println salto de liña
+//        concatenación String
+//        System.out.println("un saudo a DAM"+"no dia de hoxe");
+        String saudo = "un saudo a DAM";
+        String hoxe="no dia de hoxe";
+        
+        System.out.println(saudo + hoxe);
+        
+       
+    }
+    
+}
