44aafeff32f8fdf59616de27c24da2bca7176f42
cfigueirasvalverde <cfigueirasvalverde@nautilus20.danielcastelao.org>
2018-11-20 09:23:33 +0100

    ramassss

---
+++ ramas/src/ramas/Ramas.java
@@ -0,0 +1,16 @@
+package ramas;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Ramas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //ramas
+    }
+    
+}
